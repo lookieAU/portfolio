d15251317d262de47fcf5b8b36362289814864ca
lookieAU <plabandatta2015@gmail.com>
2024-06-15 20:48:39 +0530

    First

---
+++ src/app/education.js
@@ -0,0 +1,9 @@
+"use client";
+
+export default function Education(){
+    return(
+        <div>
+            <p>Hindi Higher Secondary School</p>
+        </div>
+    )
+}
--- src/app/page.js
+++ src/app/page.js
@@ -1,95 +1,53 @@
+"use client";
+
 import Image from "next/image";
 import styles from "./page.module.css";
+import { useState } from "react";
+import { getModifiedCookieValues } from "next/dist/server/web/spec-extension/adapters/request-cookies";
+import Education from "./education";
 
 export default function Home() {
+  const [activeTab, setActiveTab] = useState("education")
+
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.js</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <main>
+      <div className={styles.container}>
+        <div className={styles.left}>
+          <Image className={styles.image} src={require("../../assets/profile.png")} />
+          <div className={styles.nameContainer}>
+            <p>Plaban Datta</p>
+          </div>
+          <div>
+            <p>Electronics engineer</p>
+            <p>with interests in everything tech + neuro</p>
+            <p>+ football, among others</p>
+          </div>
+          <div className={styles.linkContainer}>
+            <a className={styles.link} href="https://linkedin.com/in/lookie">LinkedIn</a>
+            <a className={styles.link} href="https://github.com">Github</a>
+            <a className={styles.link} href="https://instagram.com">Instagram</a>
+          </div>
+        </div>
+        <div className={styles.right}>
+          <div className={styles.nav}>
+            <button onClick={() => setActiveTab("education")} className={styles.navButton}>Education</button>
+            <button onClick={() => setActiveTab("exp")} className={styles.navButton}>Experience</button>
+            <button onClick={() => setActiveTab("past")} className={styles.navButton}>Past time</button>
+            <button onClick={() => setActiveTab("volunteer")} className={styles.navButton}>Volunteering</button>
+            <button onClick={() => setActiveTab("achieve")} className={styles.navButton}>Achievements</button>
+            <button onClick={() => setActiveTab("travel")} className={styles.navButton}>Travel</button>
+            <button onClick={() => setActiveTab("publication")} className={styles.navButton}>Publications</button>
+          </div>
+          <div className={styles.content}>
+            {
+              {
+                "education": <Education />,
+                "exp": <p>exp</p>
+              }[activeTab]
+            }
+          </div>
         </div>
       </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore starter templates for Next.js.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
     </main>
-  );
+  )
 }
